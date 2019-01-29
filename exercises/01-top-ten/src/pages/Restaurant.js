e139ff27fcf0fad2887c8d1f28379c6b337183f8
enrikevee <everduzco@ENRIQUEs-MBP.localdomain>
2019-01-29 14:01:50 -0800

    started to build restaurant component

--- exercises/01-top-ten/src/pages/Restaurant.js
+++ exercises/01-top-ten/src/pages/Restaurant.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react'
 import restaurantsData from "../store/restaurantsData.js"
 
 class Restaurant extends Component {
+  constructor(props){
+    super(props)
+    //deconstructing props to match
+    const { match } = props
+
+    this.state={
+      //State is set to the current product ID
+      recipeId: match.params.id
+    }
+  }
+
+  componentDidUpdate(prevProps){
+    //assign previous props to a variable
+    const prevMatch = prevProps.match
+
+
+  }
   render() {
     console.log(this.props);
     return (
