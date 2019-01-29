4a7e637bbd66d5d97ff61e0ec0c51a0517c6fc9d
Sam Sale <sam.sale@sky.com>
2019-01-29 14:51:59 -0800

    Added resturant construcuter for stretch goal

--- exercises/01-top-ten/src/pages/Restaurant.js
+++ exercises/01-top-ten/src/pages/Restaurant.js
@@ -9,20 +9,42 @@ class Restaurant extends Component {
 
     this.state={
       //State is set to the current product ID
-      recipeId: match.params.id
+      restaurantId: match.params.id
     }
   }
 
   componentDidUpdate(prevProps){
     //assign previous props to a variable
     const prevMatch = prevProps.match
+    //deconstructing current props
+    const { match } = this.props
+    //if current id doesn't match previous id
+    if(match.params.id != prevMatch.params.id){
+      //set state to new ID
+      this.setState({restaurantId: match.params.id})
+    }
 
 
   }
   render() {
-    console.log(this.props);
+    //destructure current resturant ID
+    const { restaurantId } = this.state
+    // seach through JSON objects, and find the objects based off the current ID
+    const resturantObject = restaurantsData.find((resturant) => resturant.id == restaurantId)
     return (
-      <h1>res</h1>
+      <div>
+        <h1>{resturantObject.name}</h1>
+        <p>{resturantObject.location}</p>
+        <h3>Top Three Dishes</h3>
+        <ul>
+        {resturantObject.menuItems.map((item) =>
+          <li>
+              {item}
+          </li>
+          )}
+        </ul>
+        </div>
+
     )
   }
 }
